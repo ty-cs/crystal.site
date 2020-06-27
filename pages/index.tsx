aad4c222d8279f4486945f6bcdc86a2323e90d3b
indefinitus <18642966+indefinitus@users.noreply.github.com>
2020-06-27 19:12:47 +0800

    feat: use mui

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,82 @@ import Head from 'next/head';
 import { GetServerSideProps } from 'next';
 import axios from 'axios';
 import React, { useEffect } from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 import { IncomingMessage } from 'http';
 interface HomeProps {
   ip: string;
   extraInfo: string;
 }
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      minHeight: '100vh',
+      padding: '0 0.5rem',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+
+      '& main': {
+        padding: '5rem 0',
+        flex: '1',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      '& footer': {
+        width: '100%',
+        height: '100px',
+        borderTop: '1px solid #eaeaea',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        '& img': {
+          marginLeft: '0.5rem',
+        },
+        '& a': {
+          fontSize: '1rem',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+      },
+      '& a': {
+        color: 'inherit',
+        textDecoration: 'none',
+      },
+    },
+
+    title: {
+      margin: 0,
+      lineHeight: 1.15,
+      fontSize: '4rem',
+    },
+    subtitle: {
+      fontSize: '1.5rem',
+    },
+    logo: {
+      height: '1em',
+    },
+    table: {},
+  }),
+);
+
 export const Home: React.FC<HomeProps> = ({ ip, extraInfo }): JSX.Element => {
   useEffect(() => {
     console.log('extra', extraInfo);
   });
+  const classes = useStyles();
   return (
-    <div className="container">
+    <div className={classes.container}>
       <Head>
         <title>Crystal</title>
         <link rel="icon" href="/favicon.ico" />
@@ -20,161 +85,38 @@ export const Home: React.FC<HomeProps> = ({ ip, extraInfo }): JSX.Element => {
       </Head>
 
       <main>
-        <h1 className="title">Welcome to Crystal!</h1>
-        <h2>
+        <h1 className={classes.title}>Welcome to Crystal.</h1>
+        <h2 className={classes.subtitle}>
           Your IP: <span className="ip-addr">{ip}</span>
         </h2>
+
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Key</TableCell>
+                <TableCell align="right">Value</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {Object.entries(extraInfo).map(([k, v]) => (
+                <TableRow key={k}>
+                  <TableCell component="th" scope="row">
+                    {k}
+                  </TableCell>
+                  <TableCell align="right">{'' + v}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </main>
 
       <footer>
         <a href="https://vercel.com" target="_blank" rel="noopener noreferrer">
-          Powered by <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+          Powered by <img src="/vercel.svg" alt="Vercel Logo" className={classes.logo} />
         </a>
       </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
-            Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Roboto, Helvetica, sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   );
 };
