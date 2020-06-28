9f4c266c2f84b7a049fadf61769a8242625829ed
indefinitus <18642966+indefinitus@users.noreply.github.com>
2020-06-28 17:42:57 +0800

    feat: opt

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,7 +33,7 @@ export default function MyApp(props: AppProps) {
   return (
     <React.Fragment>
       <Head>
-        <title>My page</title>
+        <title>Crystal</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
