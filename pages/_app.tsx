8fd56350a16ca0a5fbc785968f6145f35d2d88a0
indefinitus <18642966+indefinitus@users.noreply.github.com>
2020-10-05 05:14:49 +0800

    chore: updated head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -50,6 +50,8 @@ export default function MyApp(props: AppProps) {
     <>
       <Head>
         <title>Crystal</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="network utilities for developers" />
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
