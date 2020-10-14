f679b3bd37c4b00b8a54cde4e23c356b7dba65a5
indefinitus <18642966+indefinitus@users.noreply.github.com>
2020-10-14 22:44:55 +0800

    feat: use ui-rounded on safari

--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,7 @@ import { red } from '@material-ui/core/colors';
 const theme = {
   typography: {
     fontFamily: [
+      'ui-rounded',
       'SF Pro Rounded',
       '-apple-system',
       'BlinkMacSystemFont',
